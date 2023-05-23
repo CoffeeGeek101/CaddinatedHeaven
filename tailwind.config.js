82f42ce7a004d91e1ee430c619939480cd4a8f65
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-05-23 21:23:56 +0530

    project config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      }
     },
+    screens : {
+      'xs' : {'min': '318px', 'max': '450px'},
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }
